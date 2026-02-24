1cd6707e1f6c6cf0ac3a74d815b8c733f1cb815a
NatiHermann <nataliahermann65@gmail.com>
2026-02-24 15:30:17 -0300

    criada função pra código e nome

--- main.py
+++ main.py
@@ -6,4 +6,12 @@ estoque = []
 # Função pra cadastro do produto
 
 def cadastrar_produto():
-    print("")
+    print("\n --- Cadastro de Produto ---")
+
+    codigo = input("Digite o código do produto: ")
+    for produto in estoque:
+        if produto["codigo"] == codigo:
+            print("Erro: Já existe um produto com esse código.")
+            return
+    nome = input("Digite o nome do produto: ")
+        
